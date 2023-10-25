66746451fa80a2a6637e287a2196c78f94493adb
lixjia <JiaLi@Jias-MacBook-Pro.local>
2023-10-25 11:52:29 -0400

    html and logic finish

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,5 +1,2 @@
-from src.repositories.movie_repository import get_movie_repository
-from src.models.movie import Movie
-movie_repo = get_movie_repository()
-movie1 = movie_repo.create_movie("Star Wars", "George Lucas", 4)
-movie2 = movie_repo.create_movie("Oppenheimer", "Christopher Nolan", 3)
+# TODO: Feature 3
+

fec2618b7fb1cff38d6fe54bf7534f4d1b573019
rodneyu19 <urodney19@gmail.com>
2023-10-25 20:13:27 -0400

    Added test case to view movies

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,1 +1,14 @@
 # TODO: Feature 4
+from app import app
+from src.repositories.movie_repository import get_movie_repository
+
+#Create test client
+test_app = app.test_client()
+
+# Test if the page will load
+def test_get_single_movie():
+	
+	# Simulate a GET request
+	response = test_app.get('movies/search')
+	# Check if the HTTP status code is 200, 'aka' sucessful
+	assert response.status == 200 

48b325c6569856fba6e8bcec29464fc77d367123
lixjia <JiaLi@Jias-MacBook-Pro.local>
2023-10-27 20:58:30 -0400

    edit

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -5,8 +5,6 @@ get_movie_repository().create_movie("test2", "testDirector9", 3)
 
 def test_get_title():
     
-    #test the size
-    assert len(get_movie_repository().get_all_movies()) == 2
     #test that it matches director with a known movie
     assert get_movie_repository().get_movie_by_title('test1').director == 'testDirector7'
     #test that no movie exist
